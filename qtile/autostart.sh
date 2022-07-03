8be5e02969393501ccc6d2841f55713a39d9869e
Sevensuii <44603808+sevensuii@users.noreply.github.com>
2022-07-03 19:43:31 +0100

    Add files via upload

---
+++ qtile/autostart.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+## Using feh to set the background on startup
+# You can uncomment the line below, if you are doing the setup on your own.
+# If you are running the setup script, it will add this line.
+# feh --bg-scale <Path_To_Dotfiles>/Dotfiles/Wallpapres/5120x2880.jpg
+
+## Using nitrogen to set wallpaper on startup
+#nitrogen --restore &
+
+## Starting compton compositor on startup for transparency
+# If transparency is not working, try with vsync
+picom &
+
+#################################
+# Start pulseaudio #            #
+####################            #
+                                #
+pulseaudio --start              #
+                                #
+#################################
+
+#################################################################################################
+# Start polkit authentication agent #                                                           #
+#####################################                                                           #
+                                                                                                #
+lxpolkit &                                                                                      #
+#nitrogen --set-scaled /home/sevensuii/Git-Repos/Dotfiles/Wallpapers/Mountains.jpg --save       #
+                                                                                                #
+#################################################################################################
+
+#############################
+# NM Wifi applet #          #
+##################          #
+                            #
+nm-applet &                 #
+                            #
+#############################
+
+#####################################################################################
+# Mouse options #                                                                   #
+#################                                                                   #
+                                                                                    #
+xinput --set-prop 'ASUE120A:00 04F3:319B Touchpad' 'libinput Tapping Enabled' 1     #
+                                                                                    #
+#####################################################################################
+
+#########################################################################################################################
+# Screen options #                                                                                                      #
+##################                                                                                                      #
+                                                                                                                        #
+#connected2k=false                                                                                                       #
+#dpVal=$(xrandr | grep "DP-1-0.8" | cut -d ' ' -f 2)                                                                     #
+#hdmiVal=$(xrandr | grep "HDMI-1" | cut -d ' ' -f 2)                                                                     #
+                                                                                                                        #
+# if main 2k monitor is connected                                                                                       #
+#if [[ $dpVal == 'connected' ]];                                                                                         #
+#then                                                                                                                    #
+#    xrandr --output DP-1-0.8 --primary --mode 2560x1440 --rate 144 --pos 1920x0 --rotate normal --right-of eDP-1        #
+#    connected2k=true                                                                                                    #
+#fi                                                                                                                      #
+                                                                                                                        #
+# if secondary monitor is connected                                                                                     #
+#if [[ $hdmiVal == 'connected' ]];                                                                                       #
+#then                                                                                                                    #
+#    if $connected2k;                                                                                                    #
+#    then         # checks of main 2k monitor is connected and set it on the right                                       #
+ #       xrandr --output HDMI-1 --mode 1920x1080 --pos 4480x180 --rate 75 --rotate normal --right-of DP-1-0.8            #
+#    else                                                                                                                #
+#        # if main 2k monitor is not connected                                                                           #
+  #      xrandr --output HDMI-1 --mode 1920x1080 --pos 4480x180 --rate 75 --rotate normal --right-of eDP-1               #
+#    fi                                                                                                                  #
+#fi                                                                                                                      #
+                                                                                                                        #
+######################                                                                                                  #
+# End Screen options #                                                                                                  #
+#########################################################################################################################
+
+#################################################
+# Optimus manager graphical interface #         #
+#######################################         #
+                                                #
+optimus-manager-qt &                            #
+                                                #
+#################################################
+
+
+
+
+
+
---
+++ qtile/config.py
@@ -0,0 +1,222 @@
+# Copyright (c) 2010 Aldo Cortesi
+# Copyright (c) 2010, 2014 dequis
+# Copyright (c) 2012 Randall Ma
+# Copyright (c) 2012-2014 Tycho Andersen
+# Copyright (c) 2012 Craig Barnes
+# Copyright (c) 2013 horsik
+# Copyright (c) 2013 Tao Sauvage
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from typing import List  # noqa: F401
+
+from libqtile import layout, hook, bar
+#from libqtile import widget
+from qtile_extras import widget
+from qtile_extras.bar import Bar
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+from qtile_extras.widget.decorations import RectDecoration, BorderDecoration
+
+# Importing keys and colours
+from Keybindings import *
+from Colours_Decor import *
+
+from qtile_extras.popup.toolkit import (
+PopupRelativeLayout,
+PopupImage,
+PopupText
+)
+
+import os
+import subprocess
+import psutil
+import fontawesome as fa
+
+
+@hook.subscribe.startup
+def autostart():
+    home = os.path.expanduser('~/.config/qtile/autostart.sh')
+    subprocess.run([home])
+
+#groups = [Group(i) for i in "123456789"]
+
+groups = [
+    Group("1", label=fa.icons["terminal"]),
+    Group("2", label=fa.icons["firefox"]),
+    Group("3", label=fa.icons["code"]),
+    Group("4", label=fa.icons["folder"]),
+    Group("5", label=fa.icons["github"]),
+    Group("6", label=fa.icons["soundcloud"]),
+    Group("7", label=fa.icons["gamepad"]),
+    #Group("8", label = fa.icons["firefox"]),
+    #Group("9", label = fa.icons["firefox"]),
+]
+
+for i in groups:
+    keys.extend(
+        [
+            # mod1 + letter of group = switch to group
+            Key(
+                [mod],
+                i.name,
+                lazy.group[i.name].toscreen(),
+                desc="Switch to group {}".format(i.name),
+            ),
+            # mod1 + shift + letter of group = switch to & move focused window to group
+            Key(
+                [mod, "shift"],
+                i.name,
+                lazy.window.togroup(i.name, switch_group=True),
+                desc="Switch to & move focused window to group {}".format(
+                    i.name),
+            ),
+            # Or, use below if you prefer not to switch to that group.
+            # # mod1 + shift + letter of group = move focused window to group
+            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+            #     desc="move focused window to group {}".format(i.name)),
+        ]
+    )
+
+layouts = [
+    # layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    layout.Bsp(margin=8, border_focus="#533a7b", fair=False, border_on_single=True),
+    layout.Columns(border_focus_stack=[
+                   "#533a7b", "#533a7b"], border_width=3, margin=8, border_focus="#533a7b"),
+    layout.Max(),
+    # Try more layouts by unleashing below layouts.
+    # layout.Stack(num_stacks=2),
+    # layout.Bsp(),
+    # layout.Matrix(),
+    layout.MonadTall(),
+    layout.Floating(),
+    # layout.MonadWide(),
+    # layout.RatioTile(),
+    layout.Tile(),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+]
+
+widget_defaults = dict(
+    font="JetBrains Mono",
+    fontsize=16,
+    padding=5,
+)
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        top=bar.Bar(
+            [
+                widget.TextBox("", background="#00000000", foreground=foreground_colour_icon, fontsize=28, mouse_callbacks={"Button1" : lazy.spawn("rofi -show drun")}),
+                widget.CurrentLayoutIcon(),
+                widget.CurrentLayout(**decor_layout, foreground=foreground_colour),
+                #widget.CPU(format=fa.icons["microchip"]+" {freq_current}GHz {load_percent}%",**decor_wallpaper),
+                widget.CPU(format=fa.icons["microchip"]+" {load_percent}%", foreground=foreground_colour, **decor_CPU),
+                widget.GenPollText(update_interval=600, func=lambda: " {}°C".format(subprocess.check_output(os.path.expanduser("~/.config/qtile/Scripts/cpu_temp_text")).decode("utf-8")),
+                                   background="#00000000", foreground=foreground_colour, **decor_Temp),
+                widget.Memory(background="#00000000", foreground=foreground_colour,
+                              measure_mem='G', format=fa.icons["server"] + "{MemUsed: .2f} GB", **decor_ram),
+                widget.GenPollText(update_interval=600, func=lambda: " {} RPM".format(subprocess.check_output(os.path.expanduser("~/.config/qtile/Scripts/fan_speed_text")).decode("utf-8")),
+                                   background="#00000000", foreground=foreground_colour, **decor_RPM),
+                # widget.Net(interface="wlp2s0", background="#00000000", foreground=foreground_colour,
+                #            format=fa.icons["wifi"]+" {up}", **decor_Wifi),
+                widget.Net(interface="wlo1", background="#00000000", foreground=foreground_colour,
+                           format=fa.icons["wifi"]+" {up}", **decor_Wifi),
+                ##### Spacer
+                widget.Spacer(length=bar.STRETCH),
+                ##### Spacer
+
+                widget.GroupBox(highlight_method="line", highlight_color="#00000000", foreground=foreground_colour,
+                                rounded=True, **decor_groupbox, hide_unused=False, active="#000000", margin_x=6,
+                                borderwidth=4),
+
+                ##### Spacer
+                widget.Spacer(length=bar.STRETCH),
+                ##### Spacer
+
+                widget.Prompt(**decor_ram),
+                widget.Systray(background="#00000000", icon_size=20),
+                widget.CheckUpdates(background="#00000000", foreground="#FF0000",
+                                    colour_have_updates="#000000", colour_no_updates="#000000", **decor_ram),
+                # Can also use fa.icons["microchip"], fa.icons["chart-bar"]
+                #widget.GenPollText(update_interval=1, func=lambda: "{}%".format(subprocess.check_output(os.path.expanduser("~/.config/qtile/Scripts/bat_poll")).decode("utf-8")),
+                #                   background="#00000000", foreground=foreground_colour, **decor_battery),
+                widget.Clock(format=fa.icons["calendar"] + " %d-%m-%y %a",
+                             background="#00000000", foreground=foreground_colour, **decor_Day),
+                widget.Clock(format=" %I:%M:%S %p",
+                             background="#00000000", foreground=foreground_colour, **decor_Date),
+                widget.TextBox("", background="#00000000",foreground=foreground_colour_icon, fontsize=20, mouse_callbacks={"Button1" : lazy.function(show_power_menu)}),
+            ],
+            27,
+            border_width=[0, 0, 0, 0],  # Draw top and bottom borders
+            # border_color=["ff00ff", "000000", "ff00ff", "000000"],  # Borders are magenta
+            background="#00000000",
+            #margin = [5,5,5,5],
+            # opacity=1,
+        ),
+    ),
+]
+
+
+# Drag floating layouts.
+mouse = [
+    Drag(["mod1"], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag(["mod1"], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click(["mod1"], "Button2", lazy.window.bring_to_front()),
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: List
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
+    ]
+)
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
